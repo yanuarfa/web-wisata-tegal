8b4943e7ca1d18ea76c003c119a108dd026d2e52
yanuarfa <yanuarfatur41@gmail.com>
2023-01-01 12:38:32 +0700

    [ADD] Added kontak section, javascript section, biodata section

--- script.js
+++ script.js
@@ -47,3 +47,111 @@ window.onclick = function (e) {
     document.body.style.overflow = "auto";
   }
 };
+
+// Script kalkulator
+const calculator = {
+  displayNumber: "0",
+  operator: null,
+  firstNumber: null,
+  isWaitForSecondNumber: false,
+};
+
+function updateDisplay() {
+  document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+  calculator.displayNumber = "0";
+  calculator.operator = null;
+  calculator.firstNumber = null;
+  calculator.isWaitForSecondNumber = false;
+}
+
+function inputNumber(num) {
+  if (calculator.displayNumber === "0") {
+    calculator.displayNumber = num;
+  } else {
+    calculator.displayNumber += num;
+  }
+}
+
+const buttons = document.querySelectorAll(".button");
+
+for (const button of buttons) {
+  button.addEventListener("click", function (e) {
+    const target = e.target;
+
+    if (target.classList.contains("clear")) {
+      clearCalculator();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("negative")) {
+      inverseNumber();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("equals")) {
+      performCalculation();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("operator")) {
+      handleOperator(target.innerText);
+      updateDisplay();
+    }
+
+    inputNumber(target.innerText);
+    updateDisplay();
+  });
+}
+
+function inverseNumber() {
+  if (calculator.displayNumber === "0") {
+    return;
+  }
+
+  calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function handleOperator(operator) {
+  if (!calculator.isWaitForSecondNumber) {
+    calculator.operator = operator;
+    calculator.isWaitForSecondNumber = true;
+    calculator.firstNumber = calculator.displayNumber;
+
+    calculator.displayNumber = "0";
+  } else {
+    alert("Operator sudah ditetapkan!");
+  }
+}
+
+function performCalculation() {
+  if (calculator.firstNumber == null || calculator.operator == null) {
+    alert("Anda belum menetapkan operator!");
+    return;
+  }
+
+  let result = 0;
+  if (calculator.operator === "+") {
+    result =
+      parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+  } else {
+    result =
+      parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+  }
+
+  const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result,
+  };
+
+  putHistory(history);
+  calculator.displayNumber = result;
+  renderHistory();
+}
